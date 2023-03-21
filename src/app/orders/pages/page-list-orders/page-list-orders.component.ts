1c2dfbec758e9086387a1c5f40218ae6bae84481
dnyuhm <dylanmeuriot@gmail.com>
2023-03-21 13:58:47 +0100

    Commit 25 & 26 : Projection d’un tableau dynamique

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Order } from '../../models/order';
 import { OrdersService } from '../../services/orders.service';
 
 @Component({
@@ -8,10 +9,24 @@ import { OrdersService } from '../../services/orders.service';
 })
 export class PageListOrdersComponent {
   public title: string;
+  public collection!: Order[];
+  public headers: string[];
 
   constructor(private ordersService: OrdersService) {
     this.title = 'List of orders';
-    
-    this.ordersService.collection$.subscribe((data) => console.log(data));
+    this.headers = [
+      'Type',
+      'Client',
+      'Period',
+      'Unit price',
+      'Total exc. taxes',
+      'Total Incl. taxes',
+      'State',
+    ];
+
+    this.ordersService.collection$.subscribe((data) => {
+      console.log(data);
+      this.collection = data;
+    });
   }
 }
